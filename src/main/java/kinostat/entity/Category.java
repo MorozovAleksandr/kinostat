fee388107247a2587051e5577f0c9d604b4d2e4d
MorozovAleksandr <iamaleks.morozov@gmail.com>
2024-11-14 18:04:08 +0300

    added: category entity

---
+++ src/main/java/kinostat/entity/Category.java
@@ -0,0 +1,20 @@
+package kinostat.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
