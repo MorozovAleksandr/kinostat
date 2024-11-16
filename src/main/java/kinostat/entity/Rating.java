def04ab2f364f132501c27ad6d08f51e33cdc26a
MorozovAleksandr <iamaleks.morozov@gmail.com>
2024-11-16 20:13:29 +0300

    added: rating entity

---
+++ src/main/java/kinostat/entity/Rating.java
@@ -0,0 +1,27 @@
+package kinostat.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ratings")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int rating;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+}
