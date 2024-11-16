47c8ca25d673a5e91971d0f05f9094e6367d7b54
MorozovAleksandr <iamaleks.morozov@gmail.com>
2024-11-16 20:13:09 +0300

    added: category service method

--- src/main/java/kinostat/service/CategoryService.java
+++ src/main/java/kinostat/service/CategoryService.java
@@ -5,6 +5,8 @@ import kinostat.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
     @Autowired
@@ -12,5 +14,9 @@ public class CategoryService {
 
     public Category save(Category category) {
         return categoryRepository.save(category);
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
     }
 }
